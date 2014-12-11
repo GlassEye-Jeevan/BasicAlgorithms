9db5ca686b347407e54fa9e3cad4339bb50e5ec9
Jeevan Kumar <mail@jeevankumar.in>
2014-12-11 15:30:52 -0800

    Changes:
    1. Added test case Class. No real test cases yet.

---
+++ BasicAlgorithms/test/in/jeevankumar/algorithms/SearcherTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Jeevan Kumar <mail@jeevankumar.in>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package in.jeevankumar.algorithms;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Jeevan Kumar <mail@jeevankumar.in>
+ */
+public class SearcherTest extends TestCase {
+    public SearcherTest (String testName) {
+        super(testName);
+    }
+    
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testSearcher() {
+        assertEquals("1", "1");
+    }
+    
+    public void testSearcherAgain() {
+        assertEquals("1", "2");
+    }
+}
